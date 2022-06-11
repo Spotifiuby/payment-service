00836c799b40c0aa88528371e9c8f009a392eec6
bauti <jbrodriguez@fi.uba.ar>
2022-06-11 15:46:24 -0300

    log utils

---
+++ src/utils/log.js
@@ -0,0 +1,50 @@
+const { log } = require("../config");
+const winston = require("winston");
+
+const logConfiguration = {
+    "transports": [
+        new winston.transports.File({
+            filename: "logs/api_gateway.log",
+        }),
+        new winston.transport.Console()
+    ],
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: "YYYY-MM-DD HH:mm:ss"
+        }),
+        winston.format.printf(info => `${info.level}: | ${[info.timestamp]} | ${info.message}`),
+    )
+};
+
+const logger = winston.createLogger(logConfiguration);
+
+const logInfo = function(message) {
+    if (log.info){
+        logger.info(message);
+    }
+};
+
+const logDebug = function(message) {
+    if (log.debug){
+        logger.debug(message);
+    }
+};
+
+const logWarn = function(message){
+    if (log.warn){
+        logger.warn(message);
+    }
+};
+
+const logError = function(message){
+    if (log.error){
+        logger.error(message);
+    }
+};
+
+module.exports = { 
+    logInfo,
+    logDebug,
+    logWarn,
+    logError,
+};
